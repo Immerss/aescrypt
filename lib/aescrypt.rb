4b289c5fe34e4752171c1247be886c0ec679c61a
Max Dolgih <maxim.dolgih@gmail.com>
2019-02-18 19:16:18 +0200

    Update aescrypt.rb

--- lib/aescrypt.rb
+++ lib/aescrypt.rb
@@ -55,7 +55,7 @@ module AESCrypt
   #:arg: iv => String
   #:arg: cipher_type => String
   def self.decrypt_data(encrypted_data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.decrypt
     aes.key = key
 
@@ -79,7 +79,7 @@ module AESCrypt
   #:arg: iv => String
   #:arg: cipher_type => String  
   def self.encrypt_data(data, key, iv, cipher_type)
-    aes = OpenSSL::Cipher::Cipher.new(cipher_type)
+    aes = OpenSSL::Cipher.new(cipher_type)
     aes.encrypt
     aes.key = key
 
